576f01e4af91ddf39708b6a7d69398dd9b32e78d
minnnpannn9585 <panmin677095@gmail.com>
2025-10-20 17:50:47 +0800

    level two with bug

---
+++ Assets/Scripts/InputManagerTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InputManagerTime : MonoBehaviour
+{
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            // only process letter
+            if (char.IsLetter(c))
+            {
+                GameManagerTime.Instance.ProcessInput(c);
+            }
+            // backspace key clears the whole sentence
+            //else if (c == '\b')
+            //{
+            //    GameManager.Instance.ResetCurrentLine();
+            //}
+        }
+    }
+}
